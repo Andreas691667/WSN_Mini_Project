96356d147bf79cfccbbcf18804cd11fec9f4dff1
Voggo <90676486+Voggo@users.noreply.github.com>
2024-11-07 10:35:56 +0100

    fixed break/continue

--- data_reciever_vm.py
+++ data_reciever_vm.py
@@ -6,16 +6,19 @@ for i in range(100):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         sock.connect(('localhost', 60001 + i))
+        sock.setblocking(0)
         mote_sockets.append(sock)
     except ConnectionRefusedError:
-        break
-    
+        continue  # Skip to the next socket if the connection is refused
 
 # Read data from the mote's serial output
 while True:
     for i, sock in enumerate(mote_sockets):
-        data = sock.recv(1024)
-        if data:
-            print(data.decode('utf-8'))
-            with open(f'mote_{i}_data.txt', 'a') as f:
-                f.write(data.decode('utf-8'))
+        try:
+            data = sock.recv(1024)
+            if data:
+                print(data.decode('utf-8'))
+                with open(f'mote_{i}_data.txt', 'a') as f:
+                    f.write(data.decode('utf-8'))
+        except BlockingIOError:
+            continue  # Skip to the next socket if no data is available
