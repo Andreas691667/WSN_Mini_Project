b9bdb0b906f3166bf648df606f66d88f489eaab9
Voggo <90676486+Voggo@users.noreply.github.com>
2024-11-07 10:27:40 +0100

    data reciever added

---
+++ data_reciever_vm.py
@@ -0,0 +1,21 @@
+import socket
+
+# Connect to the Cooja serial socket
+mote_sockets = []
+for i in range(100):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect(('localhost', 60001 + i))
+        mote_sockets.append(sock)
+    except ConnectionRefusedError:
+        break
+    
+
+# Read data from the mote's serial output
+while True:
+    for i, sock in enumerate(mote_sockets):
+        data = sock.recv(1024)
+        if data:
+            print(data.decode('utf-8'))
+            with open(f'mote_{i}_data.txt', 'a') as f:
+                f.write(data.decode('utf-8'))
