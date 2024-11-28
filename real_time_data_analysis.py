8bd12801893ac295a8c384089e7a257d5ee2727c
Andreas691667 <andreaskaag@hotmail.com>
2024-11-28 13:14:49 +0100

    refactor update_combined_lineplot for improved time handling and reduce animation interval

--- real_time_data_analysis.py
+++ real_time_data_analysis.py
@@ -114,11 +114,9 @@ def update_combined_lineplot(frame):
         for key, value in aggregated_data.items():
             # Plot line for aggregated values with increased thickness
             agg_values, missed_data, agg_time = zip(*(value))
-            # start_idx = max(0, len(agg_values) - window_size)
-            agg_time = list(filter(lambda x: agg_time[-1] - x < 5, agg_time))
-            start_idx = max(0, len(agg_values) - len(agg_time))
+            start_idx = max(0, len(agg_values) - window_size)
             ax2.plot(
-                agg_time,
+                agg_time[start_idx:],
                 agg_values[start_idx:],
                 marker=',',
                 markersize=10,
@@ -152,9 +150,7 @@ def update_combined_lineplot(frame):
             
         for node_id, values in data.items():
             values, time = zip(*values)
-            # start_idx = max(0, max_len - window_size)
-            time = list(filter(lambda x: time[-1] - x < 5, time))
-            start_idx = max(0, len(agg_values) - len(time))
+            start_idx = max(0, max_len - window_size)
             ax2.plot(
                 time[start_idx:],
                 values[start_idx:],
@@ -172,29 +168,6 @@ def update_combined_lineplot(frame):
     ax2.legend(loc="upper left")
     ax2.grid(True)
     
-# def update_missed_plot(frame):
-#     ax3.clear()
-#     window_size = 50
-#     if aggregated_data:
-#         _, agg_time = zip(*aggregated_data)
-#         missed_window_cum = np.cumsum(missed_data[max(0, len(missed_data) - window_size):])
-#         missed = sum(missed_data)
-#         # Plot line for cumulative missed data
-#         if missed_data:
-#             start_idx = max(0, len(missed_data) - window_size)
-#             ax3.plot(
-#                 agg_time[start_idx:],
-#                 missed_window_cum,
-#                 marker='o',
-#                 markersize=1,
-#                 linestyle='-',
-#                 color='black',
-#                 label=f"Missed Data - Total: {missed}"
-#             )
-#             # Update legend dynamically
-#         ax3.set_xlabel("Time")
-#         ax3.set_ylabel("Missed Data")
-#         ax3.legend(loc="upper left")
 
 if __name__ == "__main__":
     # Determine the latest data directory
@@ -214,8 +187,8 @@ if __name__ == "__main__":
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))  # Three subplots: box plot, missed plot and line plot
 
     # ani1 = FuncAnimation(fig, update_missed_plot, interval=1000)
-    ani1 = FuncAnimation(fig, update_box_plot, interval=1000)
-    ani2 = FuncAnimation(fig, update_combined_lineplot, interval=1000)
+    ani1 = FuncAnimation(fig, update_box_plot, interval=100)
+    ani2 = FuncAnimation(fig, update_combined_lineplot, interval=100)
 
     plt.tight_layout()
     plt.show()
