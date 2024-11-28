577d219f39423e99368458011cf80bb7b65bc906
Andreas691667 <andreaskaag@hotmail.com>
2024-11-28 12:42:51 +0100

    add message aggregation structure and print function

--- contiki-ng/mini_project/data_aggregation/message.c
+++ contiki-ng/mini_project/data_aggregation/message.c
@@ -5,6 +5,12 @@ typedef struct {
     double mean;
 } message_t;
 
+typedef struct {
+    uint16_t node_id;
+    double sample;
+    int missed_samples;
+} message_agg_t;
+
 void print_message(message_t node)
 {
     // handle doubles
@@ -14,5 +20,15 @@ void print_message(message_t node)
     double b_variance = (node.variance - a_variance) * 1000;
 
     // print message
-    printf("node_id: %d, sample: %d, mean: %d.%03u, variance: %d.%03u #\n", node.node_id, node.sample, a_mean, (unsigned int)b_mean, a_variance, (unsigned int)b_variance);
+    printf("source, node_id: %d, sample: %d, mean: %d.%03u, variance: %d.%03u #\n", node.node_id, node.sample, a_mean, (unsigned int)b_mean, a_variance, (unsigned int)b_variance);
+}
+
+void print_message_agg(message_agg_t node)
+{
+    // handle doubles
+    int a_sample = node.sample;
+    double b_sample = (node.sample - a_sample) * 1000;
+
+    // print message
+    printf("aggregator, node_id: %d, sample: %d.%03u, missed_samples: %d #\n", node.node_id, a_sample, (unsigned int)b_sample, node.missed_samples);
 }
